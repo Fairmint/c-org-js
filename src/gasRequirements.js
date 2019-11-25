bfb51e1830ef146ec716975456c4d97a0a3665ef
NickCuso <hardlydiff@gmail.com>
2019-11-25 11:06:40 -0800

    Add gas constants

---
+++ src/gasRequirements.js
@@ -0,0 +1,23 @@
+module.exports = {
+  DecentralizedAutonomousTrust: {
+    approve: 48000,
+    burn: 64000,
+    buy: 190000,
+    close: 66000,
+    decreaseAllowance: 33000,
+    increaseAllowance: 48000,
+    initialize: 370000,
+    pay: 120000,
+    sell: 87000,
+    transfer: 59000,
+    transferFrom: 68000,
+    updateConfig: 210000
+  },
+  USDC: {
+    approve: 51000,
+    transfer: 40000
+  },
+  Whitelist: {
+    approve: 48000
+  }
+};
