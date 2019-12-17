7ce9b340793283d34b0436edb7d365105d72b7d1
Hardly Difficult <hardlydiff@gmail.com>
2019-12-17 15:40:17 -0800

    Split deploy steps (#65)

--- src/c-org.js
+++ src/c-org.js
@@ -5,7 +5,6 @@ const { helpers } = require("hardlydifficult-ethereum-contracts");
 async function getDat(web3, datAddress) {
   return await helpers.truffleContract.at(web3, cOrgAbi.dat, datAddress);
 }
-
 async function getWhitelist(web3, whitelistAddress) {
   return await helpers.truffleContract.at(
     web3,
@@ -13,11 +12,9 @@ async function getWhitelist(web3, whitelistAddress) {
     whitelistAddress
   );
 }
-
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-
 async function waitForDeploy(tx) {
   const hash = await tx;
   let receipt;
@@ -27,7 +24,6 @@ async function waitForDeploy(tx) {
   } while (receipt === null);
   return receipt.contractAddress;
 }
-
 function deployContract(web3, from, abi, options) {
   return new Promise((resolve, reject) => {
     const txObj = new web3.eth.Contract(abi).deploy(options);
@@ -46,7 +42,6 @@ function deployContract(web3, from, abi, options) {
     });
   });
 }
-
 function deployDatTemplate(web3, from) {
   return deployContract(web3, from, cOrgAbi.dat, { data: cOrgBytecode.dat });
 }
@@ -66,12 +61,95 @@ function deployProxy(web3, from, templateAddress, adminAddress) {
     arguments: [templateAddress, adminAddress, "0x"]
   });
 }
+async function initializeDat(web3, from, datProxyAddress, options) {
+  const callOptions = Object.assign(
+    {
+      initReserve: "42000000000000000000",
+      currency: "0x0000000000000000000000000000000000000000",
+      initGoal: "0",
+      buySlopeNum: "1",
+      buySlopeDen: "100000000000000000000",
+      investmentReserveBasisPoints: "1000",
+      name: "FAIR token",
+      symbol: "FAIR"
+    },
+    options
+  );
+  const dat = await getDat(web3, datProxyAddress);
+  await dat.initialize(
+    callOptions.initReserve,
+    callOptions.currency,
+    callOptions.initGoal,
+    callOptions.buySlopeNum,
+    callOptions.buySlopeDen,
+    callOptions.investmentReserveBasisPoints,
+    callOptions.name,
+    callOptions.symbol,
+    { from }
+  );
+}
+async function updateDat(web3, from, datProxyAddress, options) {
+  const callOptions = Object.assign(
+    {
+      revenueCommitmentBasisPoints: "1000",
+      feeBasisPoints: "0",
+      burnThresholdBasisPoints: false,
+      minInvestment: "1",
+      openUntilAtLeast: "0",
+      beneficiary: options.control,
+      feeCollector: options.control
+    },
+    options
+  );
+  const dat = await getDat(web3, datProxyAddress);
+  await dat.updateConfig(
+    callOptions.whitelistAddress,
+    callOptions.beneficiary,
+    callOptions.control,
+    callOptions.feeCollector,
+    callOptions.feeBasisPoints,
+    callOptions.autoBurn,
+    callOptions.revenueCommitmentBasisPoints,
+    callOptions.minInvestment,
+    callOptions.openUntilAtLeast,
+    {
+      from
+    }
+  );
+}
+async function initializeWhitelist(
+  web3,
+  from,
+  whitelistProxyAddress,
+  datProxyAddress
+) {
+  const whitelist = await getWhitelist(web3, whitelistProxyAddress);
+  await whitelist.initialize(datProxyAddress, { from });
+}
+async function whitelistApprove(web3, from, whitelistProxyAddress, account) {
+  const whitelist = await getWhitelist(web3, whitelistProxyAddress);
+  await whitelist.approve(account, true, {
+    from: from
+  });
+}
 
 module.exports = {
+  // 1)
   deployDatTemplate,
+  // 2)
   deployWhitelistTemplate,
+  // 3)
   deployProxyAdmin,
+  // 4) 5)
   deployProxy,
+  // 6)
+  initializeDat,
+  // 7)
+  initializeWhitelist,
+  // 8-11)
+  whitelistApprove,
+  // 12)
+  updateDat,
   deploy: async (web3, options) => {
     // Once per network:
     // 1) deploy dat template
@@ -86,9 +164,9 @@ module.exports = {
     //   - enter address
     // 5) deploy whitelist proxy(whitelistTemplate.address, proxyAdmin.address)
     //   - enter address
-    // 6) whitelistProxy.initialize(datProxy.address)
+    // 6) datProxy.initialize(datFixedSettings)
     //   - display: initialized
-    // 7) datProxy.initialize(datFixedSettings)
+    // 7) whitelistProxy.initialize(datProxy.address)
     //   - display: initialized
     // 8-11) whitelist.approve(dat, control, beneficiary, feeCollector)
     // 12) datProxy.updateConfig(whitelistProxy.address, datUpdatableSettings)
@@ -99,95 +177,69 @@ module.exports = {
     // 14) proxyAdmin.transferOwnership(new control address)
     //   - no change (just display all settings)
 
-    const callOptions = Object.assign(
-      {
-        initReserve: "42000000000000000000",
-        currency: "0x0000000000000000000000000000000000000000",
-        initGoal: "0",
-        buySlopeNum: "1",
-        buySlopeDen: "100000000000000000000",
-        investmentReserveBasisPoints: "1000",
-        revenueCommitmentBasisPoints: "1000",
-        feeBasisPoints: "0",
-        burnThresholdBasisPoints: false,
-        minInvestment: "1",
-        openUntilAtLeast: "0",
-        name: "FAIR token",
-        symbol: "FAIR",
-        beneficiary: options.control,
-        feeCollector: options.control
-      },
-      options
-    );
-
     const datTemplateAddress = await waitForDeploy(
-      deployDatTemplate(web3, callOptions.control)
+      deployDatTemplate(web3, options.control)
     );
     const whitelistTemplateAddress = await waitForDeploy(
-      deployWhitelistTemplate(web3, callOptions.control)
+      deployWhitelistTemplate(web3, options.control)
     );
     const proxyAdminAddress = await waitForDeploy(
-      deployProxyAdmin(web3, callOptions.control)
+      deployProxyAdmin(web3, options.control)
     );
     const datProxyAddress = await waitForDeploy(
-      deployProxy(
-        web3,
-        callOptions.control,
-        datTemplateAddress,
-        proxyAdminAddress
-      )
+      deployProxy(web3, options.control, datTemplateAddress, proxyAdminAddress)
     );
     const whitelistProxyAddress = await waitForDeploy(
       deployProxy(
         web3,
-        callOptions.control,
+        options.control,
         whitelistTemplateAddress,
         proxyAdminAddress
       )
     );
 
+    options.whitelistAddress = whitelistProxyAddress;
+    await initializeDat(web3, options.control, datProxyAddress, options);
+
+    await initializeWhitelist(
+      web3,
+      options.control,
+      whitelistProxyAddress,
+      datProxyAddress
+    );
+    await whitelistApprove(
+      web3,
+      options.control,
+      whitelistProxyAddress,
+      datProxyAddress
+    );
+    await whitelistApprove(
+      web3,
+      options.control,
+      whitelistProxyAddress,
+      options.control
+    );
+    if (options.beneficiary && options.beneficiary !== options.control) {
+      await whitelistApprove(
+        web3,
+        options.control,
+        whitelistProxyAddress,
+        options.beneficiary
+      );
+    }
+    if (options.feeCollector && options.feeCollector !== options.control) {
+      await whitelistApprove(
+        web3,
+        options.control,
+        whitelistProxyAddress,
+        options.feeCollector
+      );
+    }
+
+    await updateDat(web3, options.control, datProxyAddress, options);
+
     const dat = await getDat(web3, datProxyAddress);
-
-    await dat.initialize(
-      callOptions.initReserve,
-      callOptions.currency,
-      callOptions.initGoal,
-      callOptions.buySlopeNum,
-      callOptions.buySlopeDen,
-      callOptions.investmentReserveBasisPoints,
-      callOptions.name,
-      callOptions.symbol,
-      { from: callOptions.control }
-    );
-
     const whitelist = await getWhitelist(web3, whitelistProxyAddress);
-    await whitelist.initialize(dat.address, { from: callOptions.control });
-    await whitelist.approve(dat.address, true, { from: callOptions.control });
-    await whitelist.approve(callOptions.beneficiary, true, {
-      from: callOptions.control
-    });
-    await whitelist.approve(callOptions.control, true, {
-      from: callOptions.control
-    });
-    await whitelist.approve(callOptions.feeCollector, true, {
-      from: callOptions.control
-    });
-
-    await dat.updateConfig(
-      whitelist.address,
-      callOptions.beneficiary,
-      callOptions.control,
-      callOptions.feeCollector,
-      callOptions.feeBasisPoints,
-      callOptions.autoBurn,
-      callOptions.revenueCommitmentBasisPoints,
-      callOptions.minInvestment,
-      callOptions.openUntilAtLeast,
-      {
-        from: callOptions.control
-      }
-    );
-
     return { dat, whitelist };
   },
   getDat,
