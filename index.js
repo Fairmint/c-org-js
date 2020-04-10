5fdb66ba601b32759c59c524c6c2c69c649f1dd7
NickCuso <hardlydiff@gmail.com>
2020-04-10 11:34:18 -0700

    Remove network search

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ module.exports = {
   constants: require("./src/constants"),
   Corg: require("./src/c-org"),
   Networks: require("./src/networks"),
-  CorgNetworkSearch: require("./src/corgNetworkSearch"),
   CorgContracts: require("./src/corgContracts"),
   GasRequirements: require("./src/gasRequirements"),
   Uniswap: require("./src/uniswap.js"),
